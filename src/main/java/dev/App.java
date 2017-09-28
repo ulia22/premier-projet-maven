9c3b0d7e2c134d5e58c483171315a2b7c083c2aa
Clément BARBASTE <barbaste.clement@gmail.com>
2017-09-28 15:54:54 +0200

    5th commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,22 +1,25 @@
 package dev;
 
 import java.io.IOException;
-
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ResourceBundle;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	String asciiArt1;
+	public static void main( String[] args )
+	{
 		try {
-			asciiArt1 = FigletFont.convertOneLine("Bonjour !");
-			 System.out.println(asciiArt1);
+			String titre = ResourceBundle.getBundle("application").getString("titre");
+			String asciiArt1 = FigletFont.convertOneLine(titre);
+			System.out.println(asciiArt1);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
-    }
+		
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+	}
 }
